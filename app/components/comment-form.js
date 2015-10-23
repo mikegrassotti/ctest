9b12b2b716fed88b779e8dfc9fb67825280a9853
mikegrassotti <mgrassotti@gmail.com>
2015-10-23 15:46:00 -0400

    call action the old fashioned way

--- app/components/comment-form.js
+++ app/components/comment-form.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
 
   actions: {
     submitComment() {
-      this.attrs.submitComment({ comment: this.get('comment') });
+      this.sendAction('submitComment', { comment: this.get('comment') });
     }
   }
 });

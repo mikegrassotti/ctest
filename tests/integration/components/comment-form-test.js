b36659d7fabcaab41da2c7a24bc46b620a4d328e
mikegrassotti <mgrassotti@gmail.com>
2015-10-23 15:44:13 -0400

    stub action the way ember-cli generator recommends

--- tests/integration/components/comment-form-test.js
+++ tests/integration/components/comment-form-test.js
@@ -7,10 +7,9 @@ moduleForComponent('comment-form', 'Integration | Component | comment form', {
 
 test('should trigger external action on form submit', function(assert) {
 
-  // test double for the external action
-  this.set('externalAction', (attributes) => assert.deepEqual(attributes, { comment: 'You are not a wizard!' }, 'submitted input value gets passed to external action'));
+  this.on('externalAction', (attributes) => assert.deepEqual(attributes, { comment: 'You are not a wizard!' }, 'submitted input value gets passed to external action'));
 
-  this.render(hbs`{{comment-form submitComment=(action externalAction)}}`);
+  this.render(hbs`{{comment-form submitComment=(action 'externalAction')}}`);
 
   // fill out the form and force an onchange
   this.$('textarea').val('You are not a wizard!');
